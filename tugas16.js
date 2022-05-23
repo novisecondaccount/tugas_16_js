e954cc1063b50a5215570971449e681cf62dfdb7
novifitri <animangap@gmail.com>
2022-05-23 20:24:48 +0700

    init

---
+++ tugas16.js
@@ -0,0 +1,7 @@
+let min = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10)
+let max = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5)
+console.log("Nilai Minimal : " +min)
+console.log("Nilai Maksimal : " +max)
+
+var str = "Saya";
+console.log(str.charCodeAt(3))
